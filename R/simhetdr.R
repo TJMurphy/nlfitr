a359a643a7ae07356608b258afa6979ee3c27e7e
medtjm <medtjm@gmail.com>
2020-01-08 09:10:28 -0500

    added weighting option to curve fit

--- R/simhetdr.R
+++ R/simhetdr.R
@@ -1,40 +1,44 @@
 #' Simulate nonlinear heteroscedastic dose-response data
 #'
-#' A sandbox to simulate random heteroscedastic response data, for variances that grow as x increases. Enter a linear scaled predictor variable and additional parameter arguments, including e for the variance term. Data generating formula is derived from the general hyperbolic function: y/ymax=x^h/(x^h+k^h), where ymax = yhi - ylo. Errors in geom_smooth fitting will occasionally happen. Just re-simulate or modify parameters.
+#' A sandbox to simulate and visualize random normal heteroscedastic response data. Variances enlarge with the value of y predicted by the nonlinear model using a constant coefficeint of variation (cv). The data generating formula is derived from the general hyperbolic model: y/ymax=x^h/(x^h+k^h). Failure errors in the plot geom_smooth fitting subfunction will occasionally happen due to the random data. These are more frequent with higher cv values. Just re-simulate with modified parameter values.
 #'
-#' @param x a vector of non-exponential linear scale values, usually representing dose or concentration.
+#' @param x a vector of non-exponential linear scale values, usually representing dose or concentration, but can represent any stimulus.
 #' @param k the value of x that yields y/ymax = 0.5, usually EC50 or ED50.
 #' @param ylo the lowest expected y value, in response units.
 #' @param yhi the highest expected y value, in response units.
 #' @param h the Hill slope, a unitless slope factor; -1 > h > 1 is steeper, -1 < h < 1 is shallower. Use negative value for downward sloping response.
-#' @param sd the standard deviation of residual error, in response units.
-#' @param e a numeric value used as an exponent to simulate heteroscedastic standard deviations (sdh): sdh=sd+sqrt(x^e)
+#' @param cv the coefficient of variation for y replicates.
 #' @param reps an integer value for number of replicates
-#' @param log logical value indicating x-axis scale for plot. Default is FALSE. If TRUE, the x values are transformed using a log10 function for a log plot.
+#' @param weight logical value indicating y scale weighting. Default is FALSE. If TRUE, curve is fit using relative (1/y^2) weighting.
+#' @param log logical value. Default is FALSE. If TRUE, linear x values are transformed using a log10 function for plotting. Only for visual aesthetic.
 #'
 #' @return ggplot, data
 #' @export
 #'
 #' @examples
 #'
-#' # this will work: x = c(1, 3, 10, 30, ...)
-#' # this will not work: x = c(1e-9, 3e-9, 10e-9, 30e-9, ...)
-#' # this will not work: x = c(-9, -8.523, -8, -7.523, ...)
+#' # exponential or log-transformed x scale values will not work
+#' # do not use x = c(1e-9, 3e-9, ...) or c(-9, -8.523, ...)
 #'
 #' dose <- c(1, 3, 10, 30, 100, 300) # eg, in nM units
 #'
+#' set.seed(2345)
+#'
 #' hetdat <- simhetdr(dose, k = 35, ylo = 100, yhi = 1000,
-#' h = 1.5, sd = 30, e = 1.5, reps = 5, log = TRUE ); hetdat
+#' h = 1.0, cv = 0.10, reps = 5, weight=TRUE, log = TRUE ); hetdat
 #'
 #' hetdat$data
 #'
 #'
-simhetdr <- function(x, k, ylo, yhi, h, sd, e, reps, log=F) {
+simhetdr <- function (x, k, ylo, yhi, h, cv, reps, weight=F, log=F) {
 
-  values <- data.frame(x=rep(x,reps), sdh=sd+sqrt(x^e))
+  yp <- ylo + (yhi - ylo)*x^h/(x^h + k^h)
+
+  values <- data.frame(x=rep(x, reps), yp)
+
   y <- c()
-  sdh <- c()
-  values <- dplyr::mutate(values, y = ylo+(yhi-ylo)*x^h/(x^h+k^h) + stats::rnorm(length(x), 0, sdh))
+
+  values <- dplyr::mutate(values, y=apply(values, 1, function(x) stats::rnorm(1, x[2], cv*x[2])))
 
   ggplot2::ggplot(
     values,
@@ -42,9 +46,11 @@ simhetdr <- function(x, k, ylo, yhi, h, sd, e, reps, log=F) {
       log10(x)} else {
         x}, y)) +
     ggplot2::geom_point(size=2) +
-    ggplot2::labs(title="y = ylo+(yhi-ylo)*x^h/(x^h+k^h) + rnorm(length(x), 0, sdh)") +
+    ggplot2::labs(title="model: y=ymax*x/(x+k)") +
     ggplot2::geom_smooth(
       method=minpack.lm::nlsLM,
+      ggplot2::aes(weight = if (weight){
+        weight = 1/y^2} else {weight = 1} ),
       formula = "y ~ylo+(yhi-ylo)*x^h/(x^h+k^h)",
       method.args = list(
         start=c(yhi=yhi,
@@ -56,4 +62,3 @@ simhetdr <- function(x, k, ylo, yhi, h, sd, e, reps, log=F) {
       color="blue"
     )
 }
-
