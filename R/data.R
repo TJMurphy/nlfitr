563ff93e863e00e4b1bcd10c6d3fad6b2a0b5eb1
medtjm <TJMurphy@users.noreply.github.com>
2020-03-28 14:29:36 -0400

    fixes  message and note from check

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' Contraction of mouse aorta in response to phenylephrine
 #' @source Murphy lab
 #' @format A dataframe with 5 independent replicates:
-#' \describe{
-#'  \{PE}{Doses of phenylephrine applied, in nM units.}
-#'  \{log10PE}{Doses of phenylephrine applied, in log10 nM units.}
-#'  \{replicate}{name of independent replicate.}
-#'  \{mN}{measured contractile tension response in milliNewtons per mg dry weight.}
+#'  \describe{
+#'  \item{PE}{Doses of phenylephrine applied, in nM units.}
+#'  \item{log10PE}{Doses of phenylephrine applied, in log10 nM units.}
+#'  \item{replicate}{name of independent replicate.}
+#'  \item{mN}{measured contractile tension response in milliNewtons per mg dry weight.}
 #' }
 #' @examples
 #' \dontrun{
