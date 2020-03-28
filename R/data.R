bee337b34199858de929db20966ae48fdecd6c1c
medtjm <TJMurphy@users.noreply.github.com>
2020-03-28 14:14:07 -0400

    adds fitlindr and aoc data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Contraction of mouse aorta in response to phenylephrine
+#' @source Murphy lab
+#' @format A dataframe with 5 independent replicates:
+#' \describe{
+#'  \{PE}{Doses of phenylephrine applied, in nM units.}
+#'  \{log10PE}{Doses of phenylephrine applied, in log10 nM units.}
+#'  \{replicate}{name of independent replicate.}
+#'  \{mN}{measured contractile tension response in milliNewtons per mg dry weight.}
+#' }
+#' @examples
+#' \dontrun{
+#'  aoc
+#' }
+"aoc"
