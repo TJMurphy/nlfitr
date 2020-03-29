495458d2cdad8049cfcb26ac6ba70a7c7dad130c
Jess Hoffman <56033335+jhoff18@users.noreply.github.com>
2020-03-29 11:48:41 -0400

    fitdecay2 completed
    passed check

---
+++ R/decay2exdata.R
@@ -0,0 +1,13 @@
+#' Two phase decay example data
+#' @source simdecay2 example
+#' @format A dataframe with 5 independent replicates:
+#'  \describe{
+#'  \item{Time}{Units of time, x.}
+#'  \item{Response}{Response units, y.}
+#'  \item{Replicate}{name of independent replicate.}
+#' }
+#' @examples
+#' \dontrun{
+#'  decay2ex
+#' }
+"decay2ex"
