20ed119c4029ebb6708c8794be7cbe246f743b45
Jess Hoffman <56033335+jhoff18@users.noreply.github.com>
2020-03-29 11:26:10 -0400

    fitdecay1 completed
    check passed

--- R/fitdecay1.R
+++ R/fitdecay1.R
@@ -16,16 +16,14 @@
 #' @export
 #'
 #' @examples
-#' x <- decay1example#Time
-#' y <- decay1example$Response
-#' # fitdecay1(x=x y=y, data=decay1example, k=0.2, ylo=1, yhi=100, weigh=T)
+#' # fitdecay1(x=Time y=Response, data=decay1ex, k=0.2, ylo=1, yhi=100, weigh=T)
 #'
 fitdecay1 <- function(x, y, data, k, ylo, yhi, weigh){
-  
+
 
   x <- substitute(x)
 
-  formula <- paste(substitute(y), "~ (yhi-ylo)*exp(-1*k*", x), "+ylo", sep="")
+  formula <- paste(substitute(y), "~ (yhi-ylo)*exp(-1*k*", x, ")+ylo", sep="")
 
   y <- eval(substitute(y), data)
 
@@ -34,32 +32,19 @@ fitdecay1 <- function(x, y, data, k, ylo, yhi, weigh){
   data <- dplyr::bind_cols(data, weight=weight)
 
   if (weigh) {
-    
+
     model <- minpack.lm::nlsLM(formula,
                                start=list(k=k, ylo=ylo, yhi=yhi),
                                data=data,
                                weights = weight)
     model
   }
-  
+
   else {
-    
+
     model <- minpack.lm::nlsLM(formula,
                                start=list(k=k, ylo=ylo, yhi=yhi),
                                data=data)
     model
   }
 }
-
-#time <- c(1, 5, 10, 15, 20, 25, 30, 35, 40, 45)
-#decay1dat <- simdecay1(time, k = 0.15, ylo = 1, yhi = 100, cv = 0.20, reps = 5)
-#fitdecay1(x=x, y=y, data=decay1dat$data, k=0.2, ylo=1, yhi=100, weigh=T)
-
-#a = decay1dat$data
-#a = a[,c(1,3)]
-#colnames(a) = c("Time", "Response")
-#a = a[order(a$Time),]
-#a = data.frame(a, Replicate = rep(paste("V",1:5,sep=""),10))
-x = a$Time
-y = a$Response
-fitdecay1(x=x, y=y, data=a, k=0.2, ylo=1, yhi=100, weigh=T)
